a162bcce4728e44e0cdf47150621e541e24684df
Raul Villarreal <rvillarrcal@cnm.edu>
2016-06-07 22:17:32 -0600

    Added the .gitignore files to this project. Completed the PHP setup, though my website doesn't get displayed in Chrome.  It says they don't find the server rvillarrcal/prework Change the project name, took the upper P out

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 	<html>
 		<head>
-			<title>I completed the Prework</title>
+			<title>I completed the prework</title>
 		</head>
 		<body>
-			<p>I completed the Prework!</p>
+			<p>I completed the prework!</p>
 		</body>
 	</html>
